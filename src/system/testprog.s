e7b5b33fba8148da5028fedd31bae42fb64b2397
patrickgue <me@patrickguenthard.ch>
2019-09-19 13:04:46 +0200

    Fixed bugs in reference tables - memory table was missing indirect y and accumulator modes. This effected the propper assembly of zpg,X and following address modes. - opcode JSR was missing from implied ops list - the opcodes for STA zpg,X and STA zpg,Y were switched

--- src/system/testprog.s
+++ src/system/testprog.s
@@ -3,12 +3,16 @@ start:
 	LDA #$05
 	STA $00
 loop:	
+	JSR [store]
 	DEC $00
 	BNE [loop]
-	JMP [forward]
+	BRK
 
-
-	.pc $0300
-forward:
-	LDA #$20
-	STA $01
+	.pc $0210
+store:
+	LDA $00
+	LDX $02
+	STA $04,X
+	INX
+	STX $02
+	RTS
