6e3ddf9e60fd03dfa9c2df92dbe411b5a77cdc5d
patrickgue <me@patrickguenthard.ch>
2019-10-06 14:34:42 +0200

    added license information as comments are now available in assembler

--- src/system/testprog.s
+++ src/system/testprog.s
@@ -1,3 +1,20 @@
+;;; testprog.s  Program to test assembler and emulator functionality
+;;; Copyright (C) 2019 Patrick Günthard
+;;; 
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;;   (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 print_char = $fa00
 char_store = $0300
 	.pc $0200
